e2268f2c6a4a1a1f0e8ea72a5c1edc9fb01f9a76
Brett <ginsutime78@gmail.com>
2021-04-08 16:50:40 -0500

    Added import random, skeleton of ScatterObj class, UI spinbox refactors
    Added import random, created a skeleton implementation of ScatterObject class that is not yet hooked up to UI class. Revised defs for x/y/zrot_field_ui to call new set_x/y/zrot_spinbox def that ended up changing QLineEdit in favor of QSpinBox and DoubleSpinBox. This was done since these boxes have built-in checks for what can't be entered into the field (AKA letters, symbols, numbers under min or over max - removes need to use lots of exception checking later)

--- src/scatter.py
+++ src/scatter.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 from PySide2 import QtWidgets, QtCore
 from shiboken2 import wrapInstance
@@ -6,7 +7,6 @@ import maya.OpenMayaUI as omui
 import maya.cmds as cmds
 import pymel.core as pmc
 from pymel.core.system import Path
-import os
 
 log = logging.getLogger(__name__)
 
@@ -18,7 +18,7 @@ def maya_main_window():
 
 
 class ScatterUI(QtWidgets.QDialog):
-    """Smart Save UI Class"""
+    """Scatter Tool UI Class"""
 
     def __init__(self):
         super(ScatterUI, self).__init__(parent=maya_main_window())
@@ -82,64 +82,119 @@ class ScatterUI(QtWidgets.QDialog):
         layout = QtWidgets.QGridLayout()
         self.x_min_lbl = QtWidgets.QLabel("X Rotation Variation Minimum")
         self.x_max_lbl = QtWidgets.QLabel("X Rotation Variation Maximum")
-        self.xrot_min = QtWidgets.QLineEdit()
-        self.xrot_min.setMinimumWidth(100)
-        self.xrot_max = QtWidgets.QLineEdit()
-        self.xrot_max.setMinimumWidth(100)
+        self._set_xrot_spinbox()
         layout.addWidget(self.x_min_lbl, 1, 0)
         layout.addWidget(self.xrot_min, 2, 0)
         layout.addWidget(self.x_max_lbl, 1, 1)
         layout.addWidget(self.xrot_max, 2, 1)
         return layout
 
+    def _set_xrot_spinbox(self):
+        self.xrot_min = QtWidgets.QSpinBox()
+        self.xrot_min.setMinimum(0)
+        self.xrot_min.setMaximum(360)
+        self.xrot_min.setMinimumWidth(100)
+        self.xrot_min.setSingleStep(10)
+        self.xrot_max = QtWidgets.QSpinBox()
+        self.xrot_max.setMinimum(0)
+        self.xrot_max.setMaximum(360)
+        self.xrot_max.setValue(360)
+        self.xrot_max.setMinimumWidth(100)
+        self.xrot_max.setSingleStep(10)
+
     def _create_yrot_rand_field_ui(self):
         layout = QtWidgets.QGridLayout()
         self.y_min_lbl = QtWidgets.QLabel("Y Rotation Variation Minimum")
         self.y_max_lbl = QtWidgets.QLabel("Y Rotation Variation Maximum")
-        self.yrot_min = QtWidgets.QLineEdit()
-        self.yrot_min.setMinimumWidth(100)
-        self.yrot_max = QtWidgets.QLineEdit()
-        self.yrot_max.setMinimumWidth(100)
+        self._set_yrot_spinbox()
         layout.addWidget(self.y_min_lbl, 3, 0)
         layout.addWidget(self.yrot_min, 4, 0)
         layout.addWidget(self.y_max_lbl, 3, 1)
         layout.addWidget(self.yrot_max, 4, 1)
         return layout
 
+    def _set_yrot_spinbox(self):
+        self.yrot_min = QtWidgets.QSpinBox()
+        self.yrot_min.setMinimum(0)
+        self.yrot_min.setMaximum(360)
+        self.yrot_min.setMinimumWidth(100)
+        self.yrot_min.setSingleStep(10)
+        self.yrot_max = QtWidgets.QSpinBox()
+        self.yrot_max.setMinimum(0)
+        self.yrot_max.setMaximum(360)
+        self.yrot_max.setValue(360)
+        self.yrot_max.setMinimumWidth(100)
+        self.yrot_max.setSingleStep(10)
+
     def _create_zrot_rand_field_ui(self):
         layout = QtWidgets.QGridLayout()
         self.z_min_lbl = QtWidgets.QLabel("Z Rotation Variation Minimum")
         self.z_max_lbl = QtWidgets.QLabel("Z Rotation Variation Maximum")
-        self.zrot_min = QtWidgets.QLineEdit()
-        self.zrot_min.setMinimumWidth(100)
-        self.zrot_max = QtWidgets.QLineEdit()
-        self.zrot_max.setMinimumWidth(100)
+        self._set_zrot_spinbox()
         layout.addWidget(self.z_min_lbl, 5, 0)
         layout.addWidget(self.zrot_min, 6, 0)
         layout.addWidget(self.z_max_lbl, 5, 1)
         layout.addWidget(self.zrot_max, 6, 1)
         return layout
 
+    def _set_zrot_spinbox(self):
+        self.zrot_min = QtWidgets.QSpinBox()
+        self.zrot_min.setMinimum(0)
+        self.zrot_min.setMaximum(360)
+        self.zrot_min.setMinimumWidth(100)
+        self.zrot_min.setSingleStep(10)
+        self.zrot_max = QtWidgets.QSpinBox()
+        self.zrot_max.setMinimum(0)
+        self.zrot_max.setMaximum(360)
+        self.zrot_max.setValue(360)
+        self.zrot_max.setMinimumWidth(100)
+        self.zrot_max.setSingleStep(10)
+
     def _create_scale_rand_field_ui(self):
         layout = QtWidgets.QGridLayout()
-        self.z_min_lbl = QtWidgets.QLabel("Scale Variation Minimum")
-        self.z_max_lbl = QtWidgets.QLabel("Scale Variation Maximum")
-        self.zrot_min = QtWidgets.QLineEdit()
-        self.zrot_min.setMinimumWidth(100)
-        self.zrot_max = QtWidgets.QLineEdit()
-        self.zrot_max.setMinimumWidth(100)
-        layout.addWidget(self.z_min_lbl, 7, 0)
-        layout.addWidget(self.zrot_min, 8, 0)
-        layout.addWidget(self.z_max_lbl, 7, 1)
-        layout.addWidget(self.zrot_max, 8, 1)
+        self.scale_min_lbl = QtWidgets.QLabel("Scale Variation Minimum")
+        self.scale_max_lbl = QtWidgets.QLabel("Scale Variation Maximum")
+        self._set_scale_spinbox()
+        layout.addWidget(self.scale_min_lbl, 7, 0)
+        layout.addWidget(self.scale_min, 8, 0)
+        layout.addWidget(self.scale_max_lbl, 7, 1)
+        layout.addWidget(self.scale_max, 8, 1)
         return layout
 
+    def _set_scale_spinbox(self):
+        self.scale_min = QtWidgets.QDoubleSpinBox()
+        self.scale_min.setMinimum(0.1)
+        self.scale_min.setValue(1.0)
+        self.scale_min.setMaximum(10)
+        self.scale_min.setMinimumWidth(100)
+        self.scale_min.setSingleStep(.1)
+        self.scale_max = QtWidgets.QDoubleSpinBox()
+        self.scale_max.setMinimum(0.1)
+        self.scale_max.setValue(1.0)
+        self.scale_max.setMaximum(10)
+        self.scale_max.setMinimumWidth(100)
+        self.scale_max.setSingleStep(.1)
+
     def _create_scatter_field_headers(self):
-        self.scatter_targ_lbl = QtWidgets.QLabel("Scatter Object Target")
+        self.scatter_targ_lbl = QtWidgets.QLabel("Object Being Scattered")
         self.scatter_targ_lbl.setStyleSheet("font: bold")
-        self.scatter_obj_lbl = QtWidgets.QLabel("Object Being Scattered")
+        self.scatter_obj_lbl = QtWidgets.QLabel("Scatter Destination Object")
         self.scatter_obj_lbl.setStyleSheet("font: bold")
         layout = QtWidgets.QGridLayout()
         layout.addWidget(self.scatter_targ_lbl, 0, 0)
         layout.addWidget(self.scatter_obj_lbl, 0, 3)
         return layout
+
+
+class ScatterObject(object):
+    """Functionality to scatter UI and random rotation/scale"""
+    def __init__(self):
+        self._create_scatter_randomization()
+
+    def _create_scatter_randomization(self):
+        xRot = random.uniform(0, 360)
+        yRot = random.uniform(0, 360)
+        zRot = random.uniform(0, 360)
+        "cmds.rotate(xRot, yRot, zRot, scatterObject)"
+        scaleFactor = random.uniform(0.8, 1.2)
+        "cmds.scale(scaleFactor, scaleFactor, scaleFactor, scatterObject)"
